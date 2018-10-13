a6e48853de133182dc0c8cf69218e8bbdc15d72b
Justdoitlast <32519529+Justdoitlast@users.noreply.github.com>
2018-10-13 17:58:39 +0700

    Update jav.js

--- jav.js
+++ jav.js
@@ -8,46 +8,49 @@
         if (!standardbrowser){
         var clockobj=document.getElementById? document.getElementById("digitalclock") : document.all.digitalclock;
         var Digital=new Date();
-        var hours=Digital.getHours();
         var minutes=Digital.getMinutes();
-        var dn="AM";
+        var hour=Digital.getHours();
+        var seconds=Digital.getSeconds();
         }
-        
-        if (hours==12){
-         dn="PM";
-        }
-        if (hours>12){
-        dn="PM";
-        hours=hours-12;
-        }
-        if (hours==0){
-         hours=12;
-        }
-        if (hours.toString().length==1){
-        hours="0"+hours;
-        }
-        if (minutes<=9){
-        minutes="0"+minutes;
-        }
+
         
         if (standardbrowser){
         if (alternate==0){
-        document.tick.tock.value=hours+" : "+minutes+" "+dn;
+        document.tick.tock.value=hour+" : "+minutes+" ";
         }
         else{
-        document.tick.tock.value=hours+"   "+minutes+" "+dn;
+        document.tick.tock.value=hour+" "+minutes+" ";
         }
         }
         else{
         if (alternate==0){
-        clockobj.innerHTML=hours+"<font color='lime'> : </font>"+minutes+" "+"<sup style='font-size:1px'>"+dn+"</sup>";
+        clockobj.innerHTML=hour+"<font color='lime'> : </font>"+minutes+"<font color='lime'> : </font>"+seconds;
         }
         else{
-        clockobj.innerHTML=hours+"<font color='black'> : </font>"+minutes+" "+"<sup style='font-size:1px'>"+dn+"</sup>";
+        clockobj.innerHTML=hour+"<font color='black'> : </font>"+minutes+"<font color='black'> : </font>"+seconds;
         }
         }
         alternate=(alternate==0)? 1 : 0;
         setTimeout("show()",1000);
         }
         window.onload=show;
+
+
+
+        // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
